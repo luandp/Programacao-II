ea83a78b9fb04247c8e9c1c8eb0ea47cac2dde19
luandp <34364105+luandp@users.noreply.github.com>
2019-11-19 01:58:25 -0300

    commit
    commit

--- ListaExercicio-03-ArrayArrayList-Matheus/src/listaexercicio/pkg03/arrayarraylist/matheus/ListaExercicio03ArrayArrayListMatheus.java
+++ ListaExercicio-03-ArrayArrayList-Matheus/src/listaexercicio/pkg03/arrayarraylist/matheus/ListaExercicio03ArrayArrayListMatheus.java
@@ -57,3 +57,4 @@ public class ListaExercicio03ArrayArrayListMatheus {
     }
     
 }
+ 
--- ListaExercicio-03-ArrayEArrayList-Matheus/src/listaexercicio/pkg03/arrayearraylist/matheus/Importados/LivroNotasArray.java
+++ ListaExercicio-03-ArrayEArrayList-Matheus/src/listaexercicio/pkg03/arrayearraylist/matheus/Importados/LivroNotasArray.java
@@ -10,5 +10,35 @@ package listaexercicio.pkg03.arrayearraylist.matheus.Importados;
  * @author Luan Paulo
  */
 public class LivroNotasArray {
-    
+ 
+	
+	private int numNotas;
+	private double somaNotas;
+	
+	public LivroNotas() {
+		this.numNotas = 0;
+		this.somaNotas = 0;
+	}
+	
+	public void adicionaNota(double nota){
+		numNotas++;
+		somaNotas += nota;
+	}
+	
+	public double calculaMedia() {
+		double media = 0;
+		if(numNotas > 0) {
+			media = somaNotas/this.numNotas;
+		}
+		return media;
+	}
+ 	
+	public int getNumNotas() {
+		return numNotas;
+	}
+
+	public double getSomaNotas() {
+		return somaNotas;
+	}
+		   
 }
