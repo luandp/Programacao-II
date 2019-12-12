1021261b397b19053a2c5246f5c99d64c233f108
luandp <34364105+luandp@users.noreply.github.com>
2019-12-12 06:18:10 -0200

    aaaaa

---
+++ ListaExercicio-03-ArrayList-Novo/src/listaexercicio/pkg03/arraylist/novo/Exemplos/AppAnuncio.java
@@ -0,0 +1,10 @@
+package listaexercicio.pkg03.arraylist.novo.Exemplos;
+
+public class AppAnuncio {
+
+	public static void main(String[] argumentos) {
+			System.out.println("Anuncio de : " + argumentos[0]);
+			System.out.println("Mensagem   : " + argumentos[1]);
+	}
+
+}
---
+++ ListaExercicio-03-ArrayList-Novo/src/listaexercicio/pkg03/arraylist/novo/ListaExercicio03ArrayListNovo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listaexercicio.pkg03.arraylist.novo;
+
+import listaexercicio.pkg03.arraylist.novo.Exemplos.AppAnuncio;
+
+/**
+ *
+ * @author Luan Paulo
+ */
+public class ListaExercicio03ArrayListNovo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                //
+               AppAnuncio app = new AppAnuncio();
+    }
+    
+}
