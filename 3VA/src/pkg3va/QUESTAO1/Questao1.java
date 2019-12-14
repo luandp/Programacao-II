1bc3e8dec1b2b553f6b296ecf01696aa36dc5f7a
luandp <34364105+luandp@users.noreply.github.com>
2019-12-14 19:20:36 -0200

    aaaaa

--- 3VA/src/pkg3va/QUESTAO1/Questao1.java
+++ 3VA/src/pkg3va/QUESTAO1/Questao1.java
@@ -12,35 +12,68 @@ package pkg3va.QUESTAO1;
 public class Questao1 {
     public static void main1() {
         /*
-        Sobrecarga de métodos. Java nos permite criar vários métodos com o mesmo nome desde que tenham parâmetros diferentes. Isso é o que chamamos de sobrecarga de métodos. A sobrecarga de métodos consiste em criarmos o mesmo método com possibilidades de entradas diferentes.
+        Sobrecarga de métodos. Java nos permite criar vários métodos
+        com o mesmo nome desde que tenham parâmetros diferentes.
+        Isso é o que chamamos de sobrecarga de métodos.]
+        A sobrecarga de métodos consiste em criarmos o mesmo método
+        com possibilidades de entradas diferentes.
         */
         
         /*
-        Uma exceção em Java é um objeto da classe java. lang. Exception, ou de uma de suas subclasses. ... Quando um erro ou situação anormal é encontrado durante a execução de um método, um objeto exceção é construído, e diversos dados sobre essa ocorrência são registrados nos campos desse objeto.
+        Uma exceção em Java é um objeto da classe java. lang. 
+        Exception, ou de uma de suas subclasses. ... Quando um 
+        ou situação anormal é encontrado durante a execução de 
+        um método, um objeto exceção é construído, e diversos dados 
+        sobre essa ocorrência são registrados nos campos desse objeto.
         */
     
         /*
-        Uma instância de uma classe é um novo objeto criado dessa classe, com o operador new. Instanciar uma classe é criar um novo objeto do mesmo tipo dessa classe. Uma classe somente poderá ser utilizada após ser instanciada.
+        Uma instância de uma classe é um novo objeto criado 
+        dessa classe, com o operador new. Instanciar uma classe 
+        é criar um novo objeto do mesmo tipo dessa classe. Uma classe 
+        somente poderá ser utilizada após ser instanciada.
         */
    
         /*
-        Em Java, o construtor é definido como um método cujo nome deve ser o mesmo nome da classe e sem indicação do tipo de retorno -- nem mesmo void. O construtor é unicamente invocado no momento da criação do objeto através do operador new. O retorno do operador new é uma referência para o objeto recém-criado
+        Em Java, o construtor é definido como um método cujo nome
+        deve ser o mesmo nome da classe e sem indicação do tipo
+        de retorno -- nem mesmo void. O construtor é unicamente
+        invocado no momento da criação do objeto através do operador 
+        new. O retorno do operador new é uma referência para o objeto 
+        recém-criado
         */
         
         /*
-        Em Java, o construtor é definido como um método cujo nome deve ser o mesmo nome da classe e sem indicação do tipo de retorno -- nem mesmo void. O construtor é unicamente invocado no momento da criação do objeto através do operador new. O retorno do operador new é uma referência para o objeto recém-criado.
+        Em Java, o construtor é definido como um método cujo nome deve 
+        ser o mesmo nome da classe e sem indicação do tipo de retorno
+        -- nem mesmo void. O construtor é unicamente invocado no momento 
+        da criação do objeto através do operador new. O retorno do operador
+        new é uma referência para o objeto recém-criado.
         */
         
         /*
-        A herança é um mecanismo da Orientação a Objeto que permite criar novas classes a partir de classes já existentes, aproveitando-se das características existentes na classe a ser estendida. ... A linguagem Java permite o uso de herança simples, mas não permite a implementação de herança múltipla
+        A herança é um mecanismo da Orientação a Objeto que permite 
+        criar novas classes a partir de classes já existentes, aproveitando-se
+        das características existentes na classe a ser estendida. ... 
+        A linguagem Java permite o uso de herança simples, mas não permite 
+        a implementação de herança múltipla
         */
     
         /*
-        Classes Abstratas e Métodos Abstratos. Classes abstratas tem uma função importante na orientação a objeto em Java. ... Ou seja, uma classe abstrata pode implementar ou não um método. Os métodos abstratos definidos em uma classe abstrata devem obrigatoriamente ser implementados em uma classe concreta
+        Classes Abstratas e Métodos Abstratos. Classes abstratas 
+        tem uma função importante na orientação a objeto em Java. ...
+        Ou seja, uma classe abstrata pode implementar ou não um método.
+        Os métodos abstratos definidos em uma classe abstrata devem 
+        obrigatoriamente ser implementados em uma classe concreta
         */
     
         /*
-        Interface é um recurso da orientação a objeto utilizado em Java que define ações que devem ser obrigatoriamente executadas, mas que cada classe pode executar de forma diferente. Interfaces contém valores constantes ou assinaturas de métodos que devem ser implementados dentro de uma classe.
+        Interface é um recurso da orientação a objeto
+        utilizado em Java que define ações que devem ser 
+        obrigatoriamente executadas, mas que cada classe pode
+        executar de forma diferente. Interfaces contém valores 
+        constantes ou assinaturas de métodos que devem ser implementados 
+        de uma classe.
         */
     }
     
