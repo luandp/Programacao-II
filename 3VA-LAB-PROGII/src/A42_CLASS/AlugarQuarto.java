bd800dd2e52bb56ac983e7b56cafdc4557ba5da4
luandp <34364105+luandp@users.noreply.github.com>
2019-12-15 17:39:34 -0200

    3VA - LAB PROG2 - ESTRUTURA E VIEWS CRIADAS

---
+++ 3VA-LAB-PROGII/src/A42_CLASS/AlugarQuarto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A42_CLASS;
+
+/**
+ *
+ * @author Luan Paulo
+ */
+public class AlugarQuarto {
+        Cliente c;
+        Quarto q;
+        
+}
---
+++ 3VA-LAB-PROGII/src/A42_CLASS/Quarto.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A42_CLASS;
+
+/**
+ *
+ * @author Luan Paulo
+ */
+public class Quarto {
+    private String nome;
+    private int Status;
+
+    public Quarto(String nome, int Status) {
+        this.nome = nome;
+        this.Status = Status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int Status) {
+        this.Status = Status;
+    }
+    
+    
+}
